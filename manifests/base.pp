775dd2e24be0322bf347153e5ddd51ec3143b698
Jeff Loiselle <jeff@broadinstitute.org>
2011-12-22 15:05:47 -0500

    added creation of puppet group to dismiss an error message

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,3 +1,8 @@
+# this makes puppet and vagrant shut up about the puppet group
+group { "puppet": 
+  ensure => "present", 
+}
+
 # make sure the packages are up to date before beginning
 exec { "apt-get update":
   command => "/usr/bin/apt-get update",
